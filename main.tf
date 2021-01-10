317b7998154d994766f53dde82733d77783e6268
waqasjilani <75110689+waqasjilani@users.noreply.github.com>
2021-01-10 14:41:14 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,4 +15,7 @@ resource "aws_s3_bucket" "kyo" {
 resource "aws_instance" "waqasec2" {
   ami           = "ami-0e80a462ede03e653"
   instance_type = "t2.micro"
+  tags = {
+    Name        = "My EC2 jenks"
+    }
 }
