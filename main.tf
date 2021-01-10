16455dbb60bb754a668311f47d8559740e84a420
waqasjilani <75110689+waqasjilani@users.noreply.github.com>
2021-01-10 17:20:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "kyo" {
-  bucket = "my-tfie-test-bucket"
+  bucket = "lovelybucket"
   acl    = "private"
 
   tags = {
@@ -16,6 +16,6 @@ resource "aws_instance" "waqasec2" {
   ami           = "ami-0e80a462ede03e653"
   instance_type = "t2.micro"
   tags = {
-    Name        = "My EC2 jenks"
+    Name        = "EC2 jenkins"
     }
 }
