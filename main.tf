ea5368c00a84c89c3c07415cbb6a8b60ef626974
waqasjilani <75110689+waqasjilani@users.noreply.github.com>
2021-01-11 15:02:01 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 provider "aws" {
   region = "eu-west-2"
-  AWS_ACCESS_KEY_ID = "AKIARHUK6R2H4YIW4ISS"
-  AWS_SECRET_ACCESS_KEY = "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"
-}
+ }
 
 resource "aws_s3_bucket" "kyo" {
   bucket = "lovetheebucket"
