4d7372f5f05b3ca1bbc8d9cdb2b87fd18bf16844
waqasjilani <75110689+waqasjilani@users.noreply.github.com>
2021-01-11 15:00:29 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "eu-west-2"
+  AWS_ACCESS_KEY_ID = "AKIARHUK6R2H4YIW4ISS"
+  AWS_SECRET_ACCESS_KEY = "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"
 }
 
 resource "aws_s3_bucket" "kyo" {
