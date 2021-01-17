dcdd8ec64023315a5551b1f8229df692bc3d2d6d
waqasjilani <75110689+waqasjilani@users.noreply.github.com>
2021-01-17 10:14:35 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
  }
 
 resource "aws_s3_bucket" "kyo" {
-  bucket = "boobysexbucket"
+  bucket = "talhabhaikibalti"
   acl    = "private"
 
   tags = {
@@ -16,6 +16,6 @@ resource "aws_instance" "waqasec2" {
   ami           = "ami-0e80a462ede03e653"
   instance_type = "t2.micro"
   tags = {
-    Name        = "EC2 jenkins"
+    Name        = "EC2 talha jenkins"
     }
 }
